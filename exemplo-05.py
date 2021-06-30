f9ed9d6037fba70a3be8cfc52a3cb35ec8c1652f
Lucas Leite <70826073+leitecsleite@users.noreply.github.com>
2021-06-29 21:33:17 -0300

    python

---
+++ exemplo-05.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+x = 1; 
+y = 1000; 
+
+if x > y : 
+    print (' x é maior que y'); 
+
+if y > x : 
+    print( 'y é maior que x');     
---
+++ exemplo-07.py
@@ -0,0 +1,11 @@
+x = 1; 
+y = 2; 
+
+if x ==y: 
+    print('numeros iguais'); 
+elif y > x : 
+    print('y maior que x'); 
+elif  x < y:
+    print('x menor que y'); 
+else: 
+    print('numeros diferente'); 
